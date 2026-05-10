3b27b65f3297a1b5c275070e2816b2020ae7b7e3
Mohammed El-kabli <melkabli05@gmail.com>
2026-05-10 20:33:58 +0100

    feat(nav): rename 'À Propos' to 'Société'

--- src/app/core/navigation.config.ts
+++ src/app/core/navigation.config.ts
@@ -8,7 +8,7 @@ export interface NavLink {
 export const NAV_LINKS: NavLink[] = [
   { label: 'Accueil', path: '/', exact: true },
   { label: 'Services', path: '/services' },
-  { label: 'Références', path: '/references' },
-  { label: 'À Propos', path: '/about' },
+  //{ label: 'Références', path: '/references' },
+  { label: 'Société', path: '/about' },
   { label: 'Contact', path: '/contact' },
 ];
